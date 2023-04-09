da0459c2c8cc90509371329606d4b027a4ab8625
Tom Lonergan <tomlonergan91@gmail.com>
2023-04-09 14:51:03 +0100

    get configs

---
+++ scripts/get_configs.sh
@@ -0,0 +1,26 @@
+#!usr/bin/env/ bash
+
+# This script will extract the configuration files from the computer it is run on
+# The files will be stored in a folder named $HOSTNAME-$WHOAMI-yyyy-mm-dd-HHMMSS
+# The resulting folder will contain files with the following:
+# - /etc/hosts
+# - fish config files
+# - git config files
+# - bash config files
+# - general setup files
+# - list of explicitly installed packages from pacman, aur, and pip
+#      - you might want to edit what packages are installed on the new computer
+
+HOSTNAME=$(hostname)
+WHOAMI=$(whoami)
+BACKUPFOLDER="$HOSTNAME-$WHOAMI-$(date +%Y-%m-%d-%H:%M:%S)"
+mkdir $BACKUPFOLDER
+cp ~/.bashrc $BACKUPFOLDER/saved.bashrc
+cp ~/.config/fish/config.fish $BACKUPFOLDER
+cp ~/.gitconfig $BACKUPFOLDER/saved.gitconfig
+sudo cp /etc/hosts $BACKUPFOLDER
+echo "Saved bashrc, fish config, gitconfig, and hosts to $BACKUPFOLDER"
+
+echo $(sudo pacman -Qent) | tr " " "\n" | sed '0~2d' > $BACKUPFOLDER/packages-pacman.txt
+echo $(sudo pacman -Qemt) | tr " " "\n" | sed '0~2d' > $BACKUPFOLDER/packages-foreign.txt
+echo "Saved lists of explicitly installed packages to $BACKUPFOLDER"
